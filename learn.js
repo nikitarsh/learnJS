4f4a8b0a185662f0ed513fab04ee140a8e16a990
nikitarsh <nikitaroshmakov@gmail.com>
2022-07-21 19:18:16 +0400

    С помощью documet.write вывели текст в окно браузера из переменной str и заключили в теги html для того что бы сделать его курсивом

--- learn.js
+++ learn.js
@@ -1,5 +1,2 @@
-let side1 = prompt('Введите сторону прямоугольника A');
-let side2 = prompt('Введите сторону прямоугольника B')
-
-alert('Периметер прямоугольника равен: ' + ((Number(side1) + Number(side2)) * 2 )); 
-alert('Площадь прямоугольника равна: ' + Number(side1) * Number(side2));
+let str = 'JavaScript';
+document.write('<i>' + str + '</i>');
