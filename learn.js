b98c44786f8df99ad90d9885ec1eedf547aa1786
nikitarsh <nikitaroshmakov@gmail.com>
2022-07-21 18:25:06 +0400

    Переделали код с помощью операции инкремента и декремента

--- learn.js
+++ learn.js
@@ -1,15 +1,13 @@
-let num = 47;
-num += 7;
-num -= 18;
-num *= 10;
-num /=15;
+/*
+let num = 10;
+num = num + 1;
+num = num + 1;
+num = num - 1;
 alert(num);
-
-/* Пример до применения сокращенных операций
-let num = 47;
-num = num + 7;
-num = num - 18;
-num = num * 10;
-num = num / 15;
+*/ 
+// Операции инкремента и дикремента 
+let num = 10;
+num++;
+num++;
+num--;
 alert(num);
-*/
