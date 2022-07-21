4a9f2bc97f2dfaca57603fde523a9eb2ba826552
nikitarsh <nikitaroshmakov@gmail.com>
2022-07-21 18:18:48 +0400

    Попробовали на практике сокращенные операции

--- learn.js
+++ learn.js
@@ -1,2 +1,15 @@
-let test = '12345';
-alert(Number(test[0]) + Number(test[4]));
+let num = 47;
+num += 7;
+num -= 18;
+num *= 10;
+num /=15;
+alert(num);
+
+/* Пример до применения сокращенных операций
+let num = 47;
+num = num + 7;
+num = num - 18;
+num = num * 10;
+num = num / 15;
+alert(num);
+*/
