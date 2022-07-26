954925e180f5cf242afe6ed18be2c8a4144c61ac
nikitarsh <nikitaroshmakov@gmail.com>
2022-07-26 15:42:10 +0400

    Строковые ключи объектов

--- learn.js
+++ learn.js
@@ -1,4 +1,4 @@
 //Объекты в JS
 "use strict"; 
-let obj = {1: 'Понедельник', 2: 'Вторник', 3: 'Среда', 4: 'Четверг', 5: 'Пятница', 6: 'Суббота', 7:'Воскресение'};
-console.log(obj);
+let user = {names:'Nik', lastname:'borisov', fthname:'Nikitich'}
+console.log(user.names + ' ' + user.lastname + ' ' + user.fthname);
