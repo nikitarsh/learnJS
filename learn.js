827ee02a2df2c3059b1436720d8d4dcedaa50ff1
nikitarsh <nikitaroshmakov@gmail.com>
2022-07-26 19:22:30 +0400

    Вычисляемые свойства JS

--- learn.js
+++ learn.js
@@ -1,6 +1,13 @@
-//Массив ключей объекта JS
+//Вычисляемые свойства в JavaScript
 
 "use strict"; 
-let obj = {x: 1, y: 2, z: 3};
-let key = 'y';
-console.log(obj[key]);
+let obj = {
+	x: 1,
+	y: 2,
+	z: 3
+};
+let [key1] = 'x';
+let [key2] = 'y';
+let [key3] = 'x';
+
+console.log(obj);
