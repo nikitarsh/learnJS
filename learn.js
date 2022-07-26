23a9f92b1eacbaa1238fb0d937d382ca2f0b60a2
nikitarsh <nikitaroshmakov@gmail.com>
2022-07-26 13:05:45 +0400

    Инкремент и декремент массива

--- learn.js
+++ learn.js
@@ -1,8 +1,8 @@
 //Массивы в JS
 "use strict"; 
 let arr = [1, 8, 6, 5];
-arr[0] += '5';
-arr[1] += '0';
-arr[2] += '9';
-arr[3] += '3';
+arr[0]++;
+++arr[1];
+arr[2]--;
+--arr[3];
 console.log(arr);
