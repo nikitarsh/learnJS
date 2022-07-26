9bdd4dbc5f246936dc01f98b590047e09ac712fe
nikitarsh <nikitaroshmakov@gmail.com>
2022-07-26 13:45:13 +0400

    Оператор delete в массивах

--- learn.js
+++ learn.js
@@ -1,6 +1,6 @@
 //Массивы в JS
 "use strict"; 
-let arr = [1, 2, 3, 4, 5]
-let key1 = 1;
-let key2 = 3;
-console.log(Number(arr[key1] + arr[key2]));
+let arr = ['a', 'b', 'c', 'd', 'e'];
+delete arr[0];
+delete arr[4];
+console.log(arr.length);
