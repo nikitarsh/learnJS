d58e2e94eb2b50bd256cf079baf545354572f6a1
nikitarsh <nikitaroshmakov@gmail.com>
2022-07-26 12:36:49 +0400

    Изменение элементов массива

--- learn.js
+++ learn.js
@@ -1,4 +1,7 @@
 //Массивы в JS
 "use strict"; 
-let arr = [56, 23, 6, 7, 7, 2, 6, 5, 2, 6, 4, 3];
-console.log(arr[arr.length - 1]);
+let arr = ['a', 'b', 'c'];
+arr[0] = '1';
+arr[1] = '-2';
+arr[2] = '-3';
+console.log(arr);
