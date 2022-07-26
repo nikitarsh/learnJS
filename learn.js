a3164ea5864f18956a0931b7e31b3f2dfdd28fc0
nikitarsh <nikitaroshmakov@gmail.com>
2022-07-26 17:37:29 +0400

    Добавление элементов в объекты JS

--- learn.js
+++ learn.js
@@ -1,8 +1,10 @@
 //Изменение элементов объектов в JavaScript, возвели в квадрат каждый элемент объекта
 
 "use strict"; 
-let obj = {x: 1, y: 2, z: 3};
-obj.x = Math.pow(1, 2);
-obj.y = Math.pow(2, 2);
-obj.z = Math.pow(3, 2);
-console.log(obj);
+let cars = {};
+cars.ford = 'kuga';
+cars.renault = 'duster';
+cars.audi = 'r8';
+cars.suzuki = 'liana';
+cars.opel = 'mokka';
+console.log(cars);
