f8a83eca10026642361bda76d1451c2db8519580
nikitarsh <nikitaroshmakov@gmail.com>
2022-07-26 13:41:45 +0400

    Сумма элементов, ключи которых хранятся в переменных

--- learn.js
+++ learn.js
@@ -1,6 +1,6 @@
 //Массивы в JS
 "use strict"; 
-let arr = [1, 2, 3];
-arr.push(4);
-arr.push(5);
-console.log(arr);
+let arr = [1, 2, 3, 4, 5]
+let key1 = 1;
+let key2 = 3;
+console.log(Number(arr[key1] + arr[key2]));
