44dc76afe2a2ef090b9ad0058293b45ec101de5d
nikitarsh <nikitaroshmakov@gmail.com>
2022-07-26 12:27:43 +0400

    объединение всех элементов массива в строку

--- learn.js
+++ learn.js
@@ -1,6 +1,4 @@
 //Массивы в JS
 "use strict"; 
-let arr = [1, 2, 3];
-let result = arr.reduce((sum, current) => sum + current, 0);
-alert(result);
-console.log(result);
+let arr = ['a', 'b', 'c', 'd'];
+alert(arr.join('-'));
